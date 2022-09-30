3a4ec6d22364f7c9f50fac9f1f5ce778e826c7c2
Aayush Paigwar <90213184+Aayush2711@users.noreply.github.com>
2022-09-30 17:37:34 +0530

    Add files via upload

---
+++ even odd.c
@@ -0,0 +1,13 @@
+//Write a C Program to Check if a given Integer is Odd or Even
+
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("Enter the number:",a);
+	scanf("%d",&a);
+	if(a%2==0)
+	printf("the number is even");
+	else
+	printf("\nThe number:%d \nis odd number",a);
+}
