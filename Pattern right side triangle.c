7298820fe66e10bc39700e0cd1f0e50d27fa7d33
Aayush Paigwar <90213184+Aayush2711@users.noreply.github.com>
2022-09-30 17:56:58 +0530

    Add files via upload

---
+++ Pattern right side triangle.c
@@ -0,0 +1,25 @@
+//WAP to print right side triangle pattern
+
+//Output:
+//	*
+//	**
+//	***
+//	****
+//	*****
+
+#include<stdio.h>
+main()
+{
+	int i,j,n;
+	printf("Enter the number of rows:",n);
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for (j=1;j<=i;j++)
+		{
+			printf("* ");
+		}
+		printf("\n\n");
+		
+	}
+}
